87f9c36e042fcbf5f935f71c2f2e6f0316d80bf5
Jasom Rocha <jasomrocha27@gmail.com>
2024-01-22 15:40:18 -0300

    findAll implementation

--- src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -70,7 +70,44 @@ public class SellerDaoJDBC implements SellerDao{
 
     @Override
     public List<Seller> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement("SELECT seller.*,department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.Id ORDER BY Name");
+            
+            rs = st.executeQuery(); //Recebe o resultado em forma de tabela, usa comandos da API JDBC para percorrer a tabela
+            
+            List <Seller> list = new ArrayList<>(); //List para receber o venderor ou vendedores resultado da pesquisa na tabela
+            
+            /*
+            RESTRIÇÃO: NÃO POSSO SAIR CRIANDO VÁRIOS DEPARTAMENTOS E FAZER MEUS VENDEDORES (OBJ) APONTAREM PARA APARTAMENTOS DIFERENTES INSTACIADOS NA MEMORIA
+            POR ISSO É NECESSÁRIO CRIAR UM ED MAP PARA RECEBER OS ids DAS INSTANCIAS EXISTENTES.
+            ESTAMOS RETIRANDO INFORMAÇÕES DO BANCO DE DADOS, E LÁ SÓ EXISTE 1 DEPARTAMENTO POR ID. LEMBRE-SE.
+            */
+            
+            Map<Integer, Department> map = new HashMap<>(); //Crio um map para armazenar os departamentos, pois os objetos não podem apontar para departamentos diferentes
+            
+            //comando next() retorna um false se não houver dados na linha
+            while(rs.next()){ 
+                Department dep = map.get(rs.getInt("DepartmentId")); //Atribuo o resultado do map a variavel dep
+            
+                if(dep == null){ //caso não exista esse departamento, ele retorna null e entra nesse if
+                dep = instantiateDepartment(rs); //atribuo o valor retornado pelo função que instancia os departamentos
+                map.put(rs.getInt("DepartmentId"), dep); //incluo o id do departamento do meu departamento no meu map, pra da proxima vez ele pular o if
+                }
+                
+                Seller obj = instantiateSeller(rs, dep); //instancio o vendedor apontando para o meu dep
+                list.add(obj); //adciono meu vendedor ao list
+            }
+            return list;
+        }
+        catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     private Department instantiateDepartment(ResultSet rs) throws SQLException {
